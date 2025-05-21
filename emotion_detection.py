8b81337130aae08ca1d9e90e5ad28f68c08048cb
Misumi <misumi0921@gmail.com>
2025-05-21 10:56:18 -0400

    Task3 is completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -19,5 +20,32 @@ def emotion_detector(text_to_analyze):
     if response.status_code != 200:
         return {"error": f"Request failed with status code {response.status_code}"}
 
+
     result = response.json()
-    return result['emotionPredictions'][0]['emotion']
+
+    emotions = result['emotionPredictions'][0]['emotion']
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    output = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
+
+    return output
