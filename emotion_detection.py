057bb39b85645ae943a5c9650305957f6983dc7f
Misumi <misumi0921@gmail.com>
2025-05-21 10:37:04 -0400

    mOVED folder

---
+++ emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=input_json)
+
+    if response.status_code != 200:
+        return {"error": f"Request failed with status code {response.status_code}"}
+
+    result = response.json()
+    return result['emotionPredictions'][0]['emotion']
